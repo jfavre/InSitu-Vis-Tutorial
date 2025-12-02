454f8c801ed9a1da988e4fe832ac9b21d6beb3c2
Jean M. Favre <jfavre@cscs.ch>
2025-12-02 14:37:42 +0100

    update HowTo

--- Examples/HeatDiffusion/C++/cleanup.sh
+++ Examples/HeatDiffusion/C++/cleanup.sh
@@ -3,7 +3,7 @@ rm -rf buildAscent
 rm -rf buildCatalyst
 rm -rf $SCRATCH/Catalyst/datasets
 rm -rf $SCRATCH/Catalyst/view*png
+rm -rf datasets
 rm -rf Heat.* output.png
-rm -rf view*png
 rm -rf mesh.cycle*
 rm slurm*
